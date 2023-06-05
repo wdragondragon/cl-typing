8aea82789b282835f726343fcaebb3ee6a61ea67
zhengjinlong <1061917196@qq.com>
2023-06-06 01:28:36 +0800

    init

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,7 @@ module com.jdragon.cltyping {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+//    requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
     opens com.jdragon.cltyping to javafx.fxml;
